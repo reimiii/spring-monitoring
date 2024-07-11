9ffd16dcf8f1547572e84627a979c5243d99c0be
Hilmi AM <imiia75775@proton.me>
2024-07-11 15:53:04 +0700

    custom health

---
+++ src/main/java/franxx/code/monitoring/MyHealthIndicator.java
@@ -0,0 +1,16 @@
+package franxx.code.monitoring;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyHealthIndicator extends AbstractHealthIndicator {
+  @Override
+  protected void doHealthCheck(Health.Builder builder) throws Exception {
+    builder.status(Status.UP);
+    builder.withDetail("app", "OK");
+    builder.withDetail("error", "NO ERROR");
+  }
+}
