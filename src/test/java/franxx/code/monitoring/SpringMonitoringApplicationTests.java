a3d27ed2e499b710d61f7c0f7d7313f6ac7c3086
Hilmi AM <imiia75775@proton.me>
2024-07-11 15:14:33 +0700

    init

---
+++ src/test/java/franxx/code/monitoring/SpringMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package franxx.code.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
