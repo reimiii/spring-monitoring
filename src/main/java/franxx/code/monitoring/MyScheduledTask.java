4915ced828258edc4f44e0ab651d9542c4d0e607
Hilmi AM <imiia75775@proton.me>
2024-07-11 16:41:47 +0700

    micrometer

--- src/main/java/franxx/code/monitoring/MyScheduledTask.java
+++ src/main/java/franxx/code/monitoring/MyScheduledTask.java
@@ -1,6 +1,8 @@
 package franxx.code.monitoring;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -8,8 +10,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyScheduledTask {
 
+  @Autowired
+  private MeterRegistry meterRegistry;
+
   @Scheduled(fixedRate = 10000)
   public void hello() {
+    meterRegistry.counter("my.schedule.task").increment();
     log.info("hello world");
   }
 }
